027e1a054338a57d61ee2152266d79c00a4d3116
rohitkuk <rohit.kukreja01@gmail.com>
2023-06-11 01:06:21 +0530

    ..

--- app/main.py
+++ app/main.py
@@ -1,22 +1,34 @@
-from typing import Union
 
+from fastapi import Request
 from fastapi import FastAPI
 
 app = FastAPI()
 
+import os
+from bark import SAMPLE_RATE, generate_audio, preload_models
+from scipy.io.wavfile import write as write_wav
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["SUNO_USE_SMALL_MODELS"] = "0"
+os.environ["SUNO_OFFLOAD_CPU"] = "1"
+
+# download and load all models
+preload_models()
+
+@app.post('/')
+async def main(request: Request):
+    content_type = request.headers.get('Content-Type')
+    
+    if content_type is None:
+        return 'No Content-Type provided.'
+    elif content_type == 'application/json':
+        try:
+            json = await request.json()
+            return generate_audio(json['text_prompt'])
+        except JSONDecodeError:
+            return 'Invalid JSON data.'
+    else:
+        return 'Content-Type not supported.'
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.post("/process_text")
-async def process_text(text: str):
-    result = len(text)
-    return {"result": result}
 

8de7753456190fa5219c3fafaa608b1c823ddb11
rohitkuk <rohit.kukreja01@gmail.com>
2023-06-11 00:53:05 +0530

    ..

--- app/main.py
+++ app/main.py
@@ -13,3 +13,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/process_text")
+async def process_text(text: str):
+    result = len(text)
+    return {"result": result}
+
